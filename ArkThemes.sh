c6f7b8de112933e27d0fa2fec1a31921755d8a92
TadMSTR <69825253+TadMSTR@users.noreply.github.com>
2021-02-16 19:33:29 -0500

    ArkThemes

---
+++ ArkThemes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Notes
+#
+# https://github.com/TadMSTR/AnberThemes
+#
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/AnberThemes"
+rm -rf /tmp/AnberThemes.log
+bash AnberThemes 2>&1 | tee -a /tmp/AnberThemes.log
