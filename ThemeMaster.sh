16c5200d18b0bd64c9fb5c81b7a0e892b1643e9c
JohnIrvine1433 <john.irvine.doe@gmail.com>
2024-04-06 17:59:23 +0200

    4.8.1

--- ThemeMaster.sh
+++ ThemeMaster.sh
@@ -6,9 +6,10 @@
 #
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/ThemeMaster"
 rm -rf /tmp/ThemeMaster.log
-
+chmod +x ./ThemeMaster
+chmod +x ./tm-joypad
+chmod +x ./image-viewer
 if [ "${UI_SERVICE}" = "weston.service" ]; then
-  chmod +x ./ThemeMaster
   run './ThemeMaster 2>&1 | tee -a /tmp/ThemeMaster.log'
 else
   bash ThemeMaster 2>&1 | tee -a /tmp/ThemeMaster.log
